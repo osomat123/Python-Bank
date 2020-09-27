c2d9bf15f113e8dda013d1c4685a0702f5ff58b4
Tejpunj Raju <tejpunjraju8a@gmail.com>
2020-09-28 00:59:40 +0530

    Added more functionality and added new User Class

--- banking.py
+++ banking.py
@@ -47,6 +47,62 @@ class Card:
         check_sum = (10 - (sum(digits) % 10)) % 10
         return str(check_sum)
 
+class User:
+    def __init__(self,number,pin):
+
+        self.number = number
+        self.pin = pin
+        self.id, self.balance = self.get_info()
+
+    def get_info(self):
+        query = f'SELECT * FROM card WHERE number = "{self.number}" AND pin ="{self.pin}"'
+        cur.execute(query)
+        records = cur.fetchall()
+        conn.commit()
+
+        return (records[0][0],records[0][3])
+
+    def add_income(self):
+        print("\nEnter income: ")
+        amount = int(input())
+        self.balance += amount
+
+        query = f'UPDATE card SET balance={self.balance} WHERE number="{self.number}"'
+        cur.execute(query)
+        conn.commit()
+
+        print("Income was added!")
+
+    def transfer_money(self,other_number,amount):
+        if amount > self.balance:
+            print("Not enough money!")
+            return
+
+        # Remove money from account
+        self.balance -= amount
+        query = f'UPDATE card SET balance = {self.balance} WHERE number="{self.number}"'
+        cur.execute(query)
+        conn.commit()
+
+        # Add money to other account
+        query = f'SELECT * from card WHERE number = "{other_number}"'
+        cur.execute(query)
+        record = cur.fetchall()
+        previous_balance = record[0][3]
+        amount += previous_balance
+        query = f'UPDATE card SET balance = {amount} WHERE number = "{other_number}"'
+        cur.execute(query)
+        conn.commit()
+
+        print("Success!")
+
+    def delete_account(self):
+        query = f'DELETE FROM card WHERE number = "{self.number}"'
+        cur.execute(query)
+        conn.commit()
+
+        print("The account has been closed!")
+
 def check_details(number, pin):
     query = f'SELECT * FROM card WHERE number = "{number}" AND pin = "{pin}"'
     cur.execute(query)
@@ -54,25 +110,84 @@ def check_details(number, pin):
     conn.commit()
 
     if len(records) == 1:
-        return [records[0][1], records[0][2], records[0][3]]
+        return User(number,pin)
 
     return None
 
+def is_valid(card_number):
+    if len(card_number) != 16:
+        return False
+
+    digits = [int(digit) for digit in list(card_number[:15])]
+    for i in range(0, 15, 2):
+        digit = digits[i] * 2
+        if digit > 9:
+            digit -= 9
+        digits[i] = digit
+
+    check_sum = (10 - (sum(digits) % 10)) % 10
+
+    if check_sum != int(card_number[-1]):
+        return False
+
+    return True
+
+def in_database(card_number):
+    query = f'SELECT number FROM card WHERE number = "{card_number}"'
+    cur.execute(query)
+    records = cur.fetchall()
+    conn.commit()
+
+    if len(records) != 1:
+        return False
+
+    return True
 
 def account_page(account):
     print("\n1. Balance")
-    print("2. Log out")
+    print("2. Add Income")
+    print("3. Do Transfer")
+    print("4. Close Account")
+    print("5. Log out")
     print("0. Exit")
 
     choice = int(input())
     print()
 
     if choice == 1:
-        print("Balance:", account[2])
+        print("Balance:", account.balance)
         return account
 
-    elif choice == 2:
+    elif choice == 5:
         print("You have successfully logged out!")
+        return 1
+
+    elif choice == 2:
+        account.add_income()
+        return account
+
+    elif choice == 3:
+        print("Transfer")
+        print("Enter card number: ")
+        other_card = input()
+
+        if other_card == account.number:
+            print("You can't transfer money to the same account!")
+
+        elif not is_valid(other_card):
+            print("Probably you made a mistake in the card number. Please try again!")
+
+        elif not in_database(other_card):
+            print("Such a card does not exist.")
+
+        else:
+            print("Enter how much money you want to transfer: ")
+            amount = int(input())
+            account.transfer_money(other_card,amount)
+        return account
+
+    elif choice == 4:
+        account.delete_account()
         return 1
 
     else:
